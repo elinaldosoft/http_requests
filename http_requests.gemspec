858eaee4de9b9c80cfb33904dfa51831928ec8d2
Elinaldo Nascimento <elinaldo.java@gmail.com>
2017-07-17 15:27:15 -0300

    remove tabs spaces

--- http_requests.gemspec
+++ http_requests.gemspec
@@ -1,10 +1,10 @@
 # encoding: utf-8
 
 Gem::Specification.new do |s|
-	s.name = "http_requests"
-	s.version = "0.1"
-	s.authors = ["Elinaldo Monteiro"]
-	s.email = "elinaldo.java@gmail.com"
-	s.require_paths = ["lib"]
-	s.homepage = "https://github.com/elinaldosoft/http_requests"
+  s.name = "http_requests"
+  s.version = "0.1"
+  s.authors = ["Elinaldo Monteiro"]
+  s.email = "elinaldo.java@gmail.com"
+  s.require_paths = ["lib"]
+  s.homepage = "https://github.com/elinaldosoft/http_requests"  
 end
--- lib/requests/api.rb
+++ lib/requests/api.rb
@@ -3,18 +3,17 @@
 require 'net/http'
 
 module Api
-	class << self
-		
-		def request(method, url, **kwargs)
-			uri = URI.parse(url)
-			response = Net::HTTP.start(uri.host, uri.port) do |http|
-      	http.send_request(method, uri)
-    	end
-		end
+  class << self  
+    def request(method, url, **kwargs)
+      uri = URI.parse(url)
+      response = Net::HTTP.start(uri.host, uri.port) do |http|
+        http.send_request(method, uri)
+      end
+    end
 
-		def get(url, **params)
-			request('GET', url, params)
-		end
+    def get(url, **params)
+      request('GET', url, params)
+    end
 
-	end
+  end
 end
