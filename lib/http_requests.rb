869356ec8249524240c259810d7bd5e9a9160739
Elinaldo Nascimento <elinaldo.java@gmail.com>
2017-07-17 15:29:12 -0300

    format text

---
+++ lib/http_requests.rb
@@ -0,0 +1,15 @@
+require 'net/http'
+
+module Api
+  class << self
+    def request(method, url, **kwargs)
+      uri = URI.parse(url)
+      response = Net::HTTP.start(uri.host, uri.port) do |http|
+        http.send_request(method, uri)
+      end
+    end
+    def get(url, **params)
+      request('GET', url, params)
+    end
+  end
+end
