126bf5501316848662590300ebabc9676bfdcba2
Elinaldo Nascimento <elinaldo.java@gmail.com>
2017-07-17 15:33:48 -0300

    format

--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -2,10 +2,10 @@
 require 'requests/api'
 
 RSpec.describe Api, "#verbs" do
-	context "Verbs HTTP GET, POST, PUT, DELETE, OPTIONS, HEAD" do
-		it "GET" do
-			rsp = Api.get('http://httpbin.org/get')
-			expect(rsp.code).to eq '200'
-		end
-	end
+  context "Verbs HTTP GET, POST, PUT, DELETE, OPTIONS, HEAD"
+    it "GET" do
+      rsp = Api.get('http://httpbin.org/get')
+      expect(rsp.code).to eq '200'
+    end
+  end
 end
