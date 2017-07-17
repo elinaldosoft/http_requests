61b4b8b9961ba71cff723b72bce422bcec9bdbd0
Elinaldo Nascimento <elinaldo.java@gmail.com>
2017-07-17 14:59:57 -0300

    format spaces

--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -2,7 +2,6 @@
 require 'requests/api'
 
 RSpec.describe Api, "#verbs" do
-	include Api
 	context "Verbs HTTP GET, POST, PUT, DELETE, OPTIONS, HEAD" do
 		it "GET" do
 			rsp = Api.get('http://httpbin.org/get')
